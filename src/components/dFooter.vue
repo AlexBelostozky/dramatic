e40c48a4f54a4cc8d615afa4a001a275546ea9da
Александр Белостоцкий <belostozky1994@mail.ru>
2023-05-15 17:18:07 +0600

    Add dFooter styles

--- src/components/dFooter.vue
+++ src/components/dFooter.vue
@@ -1,6 +1,8 @@
 <template>
   <footer class="d-footer">
-    <p class="d-footer__copyright">© 2023 Dramatic. All Rights Reserved.</p>
+    <div class="d-footer__wrapper">
+      <p class="d-footer__copyright">© 2023 Dramatic. All Rights Reserved.</p>
+    </div>
   </footer>
 </template>
 
@@ -20,5 +22,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+  @import '../assets/globalStyles/styles.less';
+  .d-footer {
+    background-color: @dark-grey;
+    padding: 0;
+  }
 
+  .d-footer__wrapper {
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 41px;
+  }
+
+  .d-footer__copyright {
+    margin: 0 auto;
+    padding: 0;
+    text-align: center;
+  }
 </style>
